361bd51b76961ab2a22a8b194cf8d3552f18c9b0
yoyocheknow <911dangerous@gmail.com>
2018-08-10 09:32:55 +0800

    Update AppTest.java

--- hadooptrain/src/test/java/com/yoyocheknow/hadoop/AppTest.java
+++ hadooptrain/src/test/java/com/yoyocheknow/hadoop/AppTest.java
@@ -52,52 +52,52 @@ public class AppTest {
     /*
      *创建文件
      */
-//    @Test
-//    public void create()throws Exception{
-//        FSDataOutputStream out=fileSystem.create(new Path("/hdfsapi/test/a.txt"));
-//        out.write("hello Hadoop".getBytes());
-//        out.flush();
-//        out.close();
-//    }
+   @Test
+   public void create()throws Exception{
+       FSDataOutputStream out=fileSystem.create(new Path("/hdfsapi/test/a.txt"));
+       out.write("hello Hadoop".getBytes());
+       out.flush();
+       out.close();
+   }
     /*
      *读取hdfs文件并打印在控制台
      */
-//    @Test
-//    public void cat() throws Exception{
-//        FSDataInputStream in=fileSystem.open(new Path("/hdfsapi/test/a.txt"));
-//        IOUtils.copyBytes(in, System.out,1024);
-//        in.close();
-//    }
+   @Test
+   public void cat() throws Exception{
+       FSDataInputStream in=fileSystem.open(new Path("/hdfsapi/test/a.txt"));
+       IOUtils.copyBytes(in, System.out,1024);
+       in.close();
+   }
     /*
      *重命名
      */
-//    @Test
-//    public void rename()throws Exception{
-//        Path oldPath= new Path("/hdfsapi/test/a.txt");
-//        Path newPath=new Path("/hdfsapi/test/b.txt");
-//        fileSystem.rename(oldPath,newPath);
-//    }
+   @Test
+   public void rename()throws Exception{
+       Path oldPath= new Path("/hdfsapi/test/a.txt");
+       Path newPath=new Path("/hdfsapi/test/b.txt");
+       fileSystem.rename(oldPath,newPath);
+   }
     /*
      *从本地 上传文件到hdfs
      */
-//    @Test
-//    public void copyFromLocalFiles()throws Exception{
-//        Path localParh= new Path("D:\\知乎-编辑推荐.txt");
-//        Path newPath=new Path("/hdfsapi/test");
-//        fileSystem.copyFromLocalFile(localParh,newPath);
-//    }
+   @Test
+   public void copyFromLocalFiles()throws Exception{
+       Path localParh= new Path("D:\\知乎-编辑推荐.txt");
+       Path newPath=new Path("/hdfsapi/test");
+       fileSystem.copyFromLocalFile(localParh,newPath);
+   }
     /*
       *带进度条的上传
       */
-//    @Test
-//    public void copyFromLocalFileswithProgress()throws Exception{
-//        InputStream in =new BufferedInputStream(new FileInputStream(new File("D:\\[洋葱] 课时07：netty服务器的快速实现.mp4")));
-//       FSDataOutputStream outputStream=fileSystem.create(new Path("/hdfsapi/test/netty-vedio.mp4")
-//               , new Progressable() {
-//                   public void progress() {
-//                       System.out.print("*");
-//                   }
-//               });
-//      IOUtils.copyBytes(in,outputStream,4096);
-//    }
+   @Test
+   public void copyFromLocalFileswithProgress()throws Exception{
+       InputStream in =new BufferedInputStream(new FileInputStream(new File("D:\\[洋葱] 课时07：netty服务器的快速实现.mp4")));
+      FSDataOutputStream outputStream=fileSystem.create(new Path("/hdfsapi/test/netty-vedio.mp4")
+              , new Progressable() {
+                  public void progress() {
+                      System.out.print("*");
+                  }
+              });
+     IOUtils.copyBytes(in,outputStream,4096);
+   }
 }
